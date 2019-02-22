4cb13425b60c421d4c03929861a200a7c7073005
Alex Williams <Zizzs17@gmail.com>
2019-02-22 08:48:10 -0800

    Added styling and layout to project details page.

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -17,18 +17,59 @@ export default function ProjectDetails(props){
                 font-size: 25px;
                 text-align: center;
             }
+
+            .projectSmallerTitle {
+                text-align: center;
+                font-size: 18px;
+            }
+
+            .projectDetailsGrid {
+                display: grid;
+                grid-template-columns: 5fr 7fr;
+                text-align: center;
+            }
+
+            a {
+                color: white;
+            }
+
+            a:visited {
+                color: white;
+            }
+
+            a:hover {
+                color: #ABB1B9;
+            }
         `}</style>
             <div>
                 <p id="projectTitle">{project.name}</p>
             </div>
             <hr></hr>
-            <div>
-                <p>{project.description}</p>
-                <p>{project.madeBy}</p>
-                <p><a href={project.github}>Github Link</a> {project.hostLink.length > 5 && <a href={project.hostLink}>Hosting Link</a> }</p>
-                <p>{project.hostLink}</p>
-                <p>{project.id}</p>
+            <div className="projectDetailsGrid">
+                <div>
+                    <p className="projectSmallerTitle">Description:</p>
+                </div>
+                <div>
+                    <p>{project.description}</p>
+                </div>
             </div>
+            <div className="projectDetailsGrid">
+                <div>
+                    <p className="projectSmallerTitle">Made By:</p>
+                </div>
+                <div>
+                    <p>{project.madeBy}</p>
+                </div>
+            </div>
+            <div className="projectDetailsGrid">
+                <div>
+                    <p className="projectSmallerTitle">Links:</p>
+                </div>
+                <div>
+                    <p><a href={project.github}>Github Link</a> {project.hostLink.length > 5 && <a href={project.hostLink}>Hosting Link</a> }</p>
+                </div>
+            </div>
+            <hr />
         </div>
     );
 }
